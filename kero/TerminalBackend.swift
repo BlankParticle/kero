f3bdb49522853000c375f30a1d73fbb10b2ee6cd
EGOIST <hi@egoist.dev>
2026-07-27 19:47:12 +0800

    tweaks

--- kero/TerminalBackend.swift
+++ kero/TerminalBackend.swift
@@ -64,14 +64,14 @@ enum TerminalBackend: String, CaseIterable, Identifiable, Sendable {
                 .init(title: "Excellent performance", isPositive: true),
                 .init(title: "GPU-accelerated", isPositive: true),
                 .init(title: "Low memory footprint", isPositive: true),
-                .init(title: "Terminal graphics", isPositive: true),
+                .init(title: "Image rendering", isPositive: true),
             ]
         case .alacritty:
             [
                 .init(title: "Excellent performance", isPositive: true),
                 .init(title: "GPU-accelerated", isPositive: true),
                 .init(title: "Lower memory footprint", isPositive: true),
-                .init(title: "Kitty terminal graphics", isPositive: true),
+                .init(title: "Image rendering", isPositive: true),
             ]
         }
     }
